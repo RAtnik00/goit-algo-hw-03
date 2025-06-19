6e83f5211f4352a59ba8b92e6102aacc00172d2a
Dimon-PC\Dimon <yaremenkodmitriy22@gmail.com>
2025-06-19 14:05:47 +0200

    rename file and add new task

---
+++ FirstTask.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def get_days_from_today(date):
+    date_obj = datetime.strptime(date, '%Y-%m-%d').date()
+
+    today = datetime.today().date()
+
+    delta = today - date_obj
+    return delta.days
+
+print(f"Скільки днів пройшло: {get_days_from_today("2021-10-09")}")
