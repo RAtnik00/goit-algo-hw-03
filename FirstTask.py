52ca7c2a2959c30fa46d34ea1726bb9e42546912
Dimon-PC\Dimon <yaremenkodmitriy22@gmail.com>
2025-06-25 14:38:28 +0200

    add try except

--- FirstTask.py
+++ FirstTask.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    date_obj = datetime.strptime(date, '%Y-%m-%d').date()
-
+    try:
+        date_obj = datetime.strptime(date, '%Y-%m-%d').date()
+    except ValueError:
+        return 'Неправильний формат дати, очікуєтся формат YYYY-MM-DD'
     today = datetime.today().date()
 
     delta = today - date_obj
